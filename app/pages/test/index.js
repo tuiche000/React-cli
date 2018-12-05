003eab65d5fda4f274b17c7c73e3fd505f183ee3
shichao.cao <shichao.cao@freemud.cn>
2018-12-05 14:40:00 +0800

    modify

--- app/pages/test/index.js
+++ app/pages/test/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export default class Home extends React.Component {
+export default class Test extends React.Component {
   render() {
     return (
       <div>
